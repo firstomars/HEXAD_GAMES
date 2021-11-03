cd127ffdf5e02d5ab129d7e200576c3ef97f90bf
Craig <45445873+Coralon@users.noreply.github.com>
2021-11-03 12:20:20 +1100

    Feature (project) reworked UI manager and UI canvas
    Moved all UI components into a single canvas. Updated UI manager to correctly display current UI functionality.

--- Project/Assets/Scripts/01_GameManagers/UIManager.cs
+++ Project/Assets/Scripts/01_GameManagers/UIManager.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Panels")]
+    [SerializeField] private GameObject backgrdPnl;
+    [SerializeField] private GameObject responsePnl;
+    [SerializeField] private GameObject colourPnl;
+    [SerializeField] private GameObject entryPnl;
+    [SerializeField] private GameObject petSpeechPnl;
+    [SerializeField] private GameObject spiritLevelSldr;
+
     [Header("Buttons")]
     [SerializeField] private GameObject menuBtn;
     [SerializeField] private GameObject playBtn;
@@ -14,18 +23,20 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject playerPrefsBtn;
     [SerializeField] private GameObject quitBtn;
 
-    [Header("Play Scene UI")]
-    [SerializeField] private GameObject playUI;
 
     private void Start()
     {
         SetSplashScreenUI(true); // rework
         SetMenuScreenUI(false);
-        SetPlayScreenUI(false);
         SetAssetStagingUI(false);
         SetTimeUI(false);
         SetPlayerStatsUI(false);
         SetPlayerPrefsUI(false);
+        SetPlayUI(false);
+        SetResponsePnlUI(false);
+        SetColourPnlUI(false);
+        SetEntryPnlUI(false);
+        SetPetPnlUI(false);
     }
 
     private void SetSplashScreenUI(bool value)
@@ -37,12 +48,7 @@ public class UIManager : MonoBehaviour
     {
         playBtn.SetActive(value);
     }
-
-    private void SetPlayScreenUI(bool value)
-    {
-        playUI.SetActive(value);
-        stagingBtn.SetActive(value);
-    }
+    
 
     private void SetAssetStagingUI(bool value)
     {
@@ -64,6 +70,38 @@ public class UIManager : MonoBehaviour
         quitBtn.SetActive(value);
     }
 
+    private void SetPlayUI(bool value)
+    {
+        backgrdPnl.SetActive(value);
+        stagingBtn.SetActive(value);
+        spiritLevelSldr.SetActive(value);
+    }
+
+    private void SetResponsePnlUI(bool value)
+    {
+        responsePnl.SetActive(value);
+    }
+
+    private void SetColourPnlUI(bool value)
+    {
+        colourPnl.SetActive(value);
+    }
+
+    private void SetEntryPnlUI(bool value)
+    {
+        entryPnl.SetActive(value);
+    }
+
+    private void SetPetPnlUI(bool value)
+    {
+        petSpeechPnl.SetActive(value);
+    }
+
+    private void UpdateSpiritLevelUI(bool value)
+    {
+        
+    }
+
     public void GoToMenuScene()
     {
         GameManager.Instance.MenuScene();
@@ -76,7 +114,7 @@ public class UIManager : MonoBehaviour
         GameManager.Instance.PlayScene();
         SetSplashScreenUI(false);
         SetMenuScreenUI(false);
-        SetPlayScreenUI(true);
+        SetPlayUI(true);
     }
 
     public void GoToStagingScene()
@@ -85,6 +123,7 @@ public class UIManager : MonoBehaviour
         SetSplashScreenUI(false);
         SetMenuScreenUI(false);
         SetAssetStagingUI(true);
+        SetPlayUI(false);
     }
 
     public void GoToTimeScene()
@@ -112,5 +151,12 @@ public class UIManager : MonoBehaviour
         SetTimeUI(false);
         SetPlayerStatsUI(false);
         SetPlayerPrefsUI(true);
+    }
+
+    // Methods called from converse class
+
+    public void DisplayPetSpeechBubble(string textToDisplay)
+    {
+        SetPetPnlUI(true);
     }
 }

fce997b15554e512496f272002dc71fdcea9f682
Omar Samad <omar.samad6@gmail.com>
2021-11-23 15:20:33 +1100

    Feature (main project) : new bed models triggered

---
+++ Project/Assets/Scripts/02_Player/PlayerAnimations.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void IdleToWalk()
+    {
+        anim.SetBool("IsIdle", false);
+        Debug.Log("walking");
+    }
+
+    public void WalkToIdle()
+    {
+        anim.SetBool("IsIdle", true);
+        Debug.Log("idling");
+    }
+}
--- Project/Assets/Scripts/03_World/03_HouseWaypoints/HouseWaypointTrigger.cs
+++ Project/Assets/Scripts/03_World/03_HouseWaypoints/HouseWaypointTrigger.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class HouseWaypointTrigger : MonoBehaviour
 {
+    private PlayerAnimations PlayerAnimations;
+
+    private void Awake()
+    {
+        PlayerAnimations = GameManager.Instance.player.GetComponent<PlayerAnimations>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         AudioManager.AudioManagerInstance.StopSound("FootStep");
+        PlayerAnimations.WalkToIdle();
     }
 
     private void OnTriggerExit(Collider other)
     {
         AudioManager.AudioManagerInstance.PlaySound("FootStep");
+        PlayerAnimations.IdleToWalk();
     }
 }

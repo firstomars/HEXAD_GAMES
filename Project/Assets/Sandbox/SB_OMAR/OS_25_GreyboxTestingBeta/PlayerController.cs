24d8a0eb4467b41b8519dcd1a0038b871d945e4b
Omar Samad <omar.samad6@gmail.com>
2021-11-25 12:56:36 +1100

    Mechanics (sandbox) : greybox testing complete

---
+++ Project/Assets/Sandbox/SB_OMAR/OS_25_GreyboxTestingBeta/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Sandbox.Omar.GreyboxUpdateTesting
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+        public LayerMask whatIsGround, whatIsPlayer;
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+
+                if (Physics.Raycast(myRay, out hitInfo, 100, whatIsGround))
+                    agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}

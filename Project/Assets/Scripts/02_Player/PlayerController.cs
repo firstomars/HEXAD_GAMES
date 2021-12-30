abe6eede4cf14fdfa036ac78bcf9a7d91dc7f6a8
Omar Samad <omar.samad6@gmail.com>
2021-12-31 08:29:57 +1100

    Player Controller tweaks for portfolio

--- Project/Assets/Scripts/02_Player/PlayerController.cs
+++ Project/Assets/Scripts/02_Player/PlayerController.cs
@@ -10,7 +10,6 @@ public class PlayerController : BehaviourStateMachine
 
     private Behaviour IntroductionBehaviour;
     private Behaviour ExerciseBehaviour;
-    //private Behaviour ConverseBehaviour;
     private Behaviour EatBehaviour;
     private Behaviour StatusCheckBehaviour;
     private Behaviour BathroomBehaviour;
@@ -18,8 +17,6 @@ public class PlayerController : BehaviourStateMachine
     private Behaviour SleepBehaviour;
     private Behaviour WanderBehaviour;
     private Behaviour ViewTrophyBehaviour;
-    private Behaviour MoodBehaviour;
-    private Behaviour EmoteBehaviour;
 
     private Behaviour currentBehaviour;
     private Behaviour nextBehaviour;
@@ -27,23 +24,25 @@ public class PlayerController : BehaviourStateMachine
     #endregion
 
     #region Camera Class
+
     private GameObject camMgr;
     [HideInInspector] public CameraManager CameraManager;
+
     #endregion
 
     #region Time Controller Class
-    //time controller class
+    
     private GameObject timeCtrlr;
     [HideInInspector] public TimeController TimeController { get; private set; }
+
     #endregion
 
     #region Trophy Controller Class
-    //trophy controller class
+    
     private GameObject trophyCtrlr;
     [HideInInspector] public TrophyController TrophyController { get; private set; }
-    #endregion
 
-    [HideInInspector] public PlayerAnimations PlayerAnimations;
+    #endregion
 
     //room trigger bools
     private bool isPlayerInGym = false;
@@ -51,14 +50,16 @@ public class PlayerController : BehaviourStateMachine
     private bool isPlayerInBedroom = false;
     private bool isPlayerInBathroom = false;
     private bool isPlayerAtTrophyCabinet = false;
-
-    //action pos checks
     private bool isPlayerSleeping = false;
 
     //pet behaviour bools
+    [SerializeField] public bool hasIntroHappened = false;
     private bool isPetSleeping = false;
     private bool isReportDelivered = true;
     private bool isPetSeeking = false;
+
+    //player animations
+    [HideInInspector] public PlayerAnimations PlayerAnimations;
 
     [Header("NavMesh Settings")]
     private NavMeshAgent agent;
@@ -78,69 +79,60 @@ public class PlayerController : BehaviourStateMachine
     [SerializeField] public float minEnergyLevelToGym;
     [SerializeField] public int petBedTime;
     [SerializeField] public int petWakeUpTime;
-
-    [SerializeField] public bool hasIntroHappened = false;
-
+    
+    [Header("Player Cosmetics")]
     [SerializeField] private GameObject petBody;
     [HideInInspector] public Renderer petSkinnedMeshRenderer;
-
-    [Header("Player Cosmetics")]
-    // Gym
     [SerializeField] private GameObject Sweatband;
     [SerializeField] private GameObject RightDumbell;
     [SerializeField] private GameObject LeftDumbell;
-    //Kitchen
     [SerializeField] private GameObject UnhealthyFood;
     [SerializeField] private GameObject HealthyFood;
-
 
     private int roomCounter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerAnimations = GetComponent<PlayerAnimations>();
+        //set-up behaviour classes
+        IntroductionBehaviour   = new IntroductionBehaviour(this);
+        ExerciseBehaviour       = new ExerciseBehaviour(this);
+        EatBehaviour            = new EatBehaviour(this);
+        StatusCheckBehaviour    = new StatusCheckBehaviour(this);
+        BathroomBehaviour       = new BathroomBehaviour(this);
+        SeekBehaviour           = new SeekBehaviour(this);
+        SleepBehaviour          = new SleepBehaviour(this);
+        WanderBehaviour         = new WanderBehaviour(this);
+        ViewTrophyBehaviour     = new ViewTrophyBehaviour(this);
 
-        #region Set Up Behaviour Classes
-
-        IntroductionBehaviour = new IntroductionBehaviour(this);
-        ExerciseBehaviour = new ExerciseBehaviour(this);
-        ConverseBehaviour = new ConverseBehaviour(this);
-        EatBehaviour = new EatBehaviour(this);
-        StatusCheckBehaviour = new StatusCheckBehaviour(this);
-        BathroomBehaviour = new BathroomBehaviour(this);
-        SeekBehaviour = new SeekBehaviour(this);
-        SleepBehaviour = new SleepBehaviour(this);
-        WanderBehaviour = new WanderBehaviour(this);
-        ViewTrophyBehaviour = new ViewTrophyBehaviour(this);
-        MoodBehaviour = new MoodBehaviour(this);
-        EmoteBehaviour = new EmoteBehaviour(this);
-
-        #endregion
+        #region REGION : Get Components - PlayerStatistics, PlayerAnimations, Renderer, NavMeshAgent
 
         //set-up navmesh agent
         agent = GetComponent<NavMeshAgent>();
 
-        //set-up pet renderers
-        petSkinnedMeshRenderer = petBody.GetComponent<Renderer>();
-
-        #region Connect Camera, Time, Trophy Classes
         //connect player stats
         PlayerStatistics = playerStatsGameObject.GetComponent<PlayerStatistics>();
 
+        //set-up pet renderers
+        petSkinnedMeshRenderer = petBody.GetComponent<Renderer>();
+
+        //connect player animations class
+        PlayerAnimations = GetComponent<PlayerAnimations>();
+
+        #endregion
+
+        #region REGION : Runtime Connect - CameraManager, TimeController, TrophyController
+
         //links Camera Manager to player in run time
-        if (camMgr == null)
-            camMgr = GameManager.Instance.world.transform.GetChild(0).gameObject;
+        if (camMgr == null) camMgr = GameManager.Instance.world.transform.GetChild(0).gameObject;
         CameraManager = camMgr.GetComponent<CameraManager>();
 
         //links Time Controller to player in run time
-        if (timeCtrlr == null)
-            timeCtrlr = GameManager.Instance.world.transform.GetChild(1).gameObject;
+        if (timeCtrlr == null) timeCtrlr = GameManager.Instance.world.transform.GetChild(1).gameObject;
         TimeController = timeCtrlr.GetComponent<TimeController>();
 
         //links Tropher Controller to player in run time
-        if (trophyCtrlr == null)
-            trophyCtrlr = GameManager.Instance.world.transform.GetChild(3).gameObject;
+        if (trophyCtrlr == null) trophyCtrlr = GameManager.Instance.world.transform.GetChild(3).gameObject;
         TrophyController = trophyCtrlr.GetComponentInChildren<TrophyController>();
 
         #endregion
@@ -169,136 +161,25 @@ public class PlayerController : BehaviourStateMachine
 
         //behaviour decision logic
         RunBehaviourLogic();
-
-        //debug functions to trigger onversations or behaviours
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            DialogueManager.DialogueManagerInstance.DisplayTip("RandomTips");
-        }
-    }
-
-    private void PetStartsWalking()
-    {
-        //Debug.Log("idle to walk");
-        if (!AudioManager.AudioManagerInstance.IsAudioPlaying("FootStep"))
-            AudioManager.AudioManagerInstance.PlaySound("FootStep");
-
-        PlayerAnimations.IdleToWalk();
-    }
-
-    private void PetStopsWalking()
-    {
-        PlayerAnimations.WalkToIdle();
-        //bug.Log("Walk to Idle called");
-
-        if (AudioManager.AudioManagerInstance.IsAudioPlaying("FootStep"))
-            AudioManager.AudioManagerInstance.StopSound("FootStep");
-
-        if (isPetSleeping) FaceTarget(bedPos.position);
-        else FaceTarget(Camera.main.transform.position);
     }
 
     private void RunBehaviourLogic()
     {
-        if (agent.remainingDistance > (agent.stoppingDistance + 0.1f))
-        {
-            PetStartsWalking();
-            //Debug.Log("Navmesh agent walking");
-        }
-        else
-        {
-            PetStopsWalking();
-        }
+        //run animations and audio
+        if (agent.remainingDistance > (agent.stoppingDistance + 0.1f)) PetStartsWalking();
+        else PetStopsWalking();
 
-        if (!hasIntroHappened)
-        {
-            nextBehaviour = IntroductionBehaviour;
-        }
-        else if (isPetSleeping)
-        {
-            return;
-        }
-        else if (!isReportDelivered)
-        {
-            nextBehaviour = StatusCheckBehaviour;
-        }
-        else if(isPetSeeking)
-        {
-            nextBehaviour = SeekBehaviour;
-        }
-        else if (isPlayerInKitchen)
-        {
-            nextBehaviour = EatBehaviour;
-        }
-        else if (isPlayerInGym) 
-        {
-            //Debug.Log("gym behaviour set");
-            nextBehaviour = ExerciseBehaviour;
-        }
-        else if (isPlayerInBedroom)
-        {
-            //Debug.Log("sleep behaviour set");
-            nextBehaviour = SleepBehaviour;
-        }
-        else if (isPlayerInBathroom)
-        {
-            //Debug.Log("bathroom behaviour set");
-            nextBehaviour = BathroomBehaviour;
-        }
-        else if (isPlayerAtTrophyCabinet)
-        {
-            nextBehaviour = ViewTrophyBehaviour;
-        }
-        else
-        {
-            //Debug.Log("agent reached destination - call wander now");
-            nextBehaviour = WanderBehaviour;
-        }
-    }
-
-    //check whether input is valid movement target for player
-    private bool isClickPointOnGround(Vector3 mouseClickPos)
-    {
-        Ray myRay = Camera.main.ScreenPointToRay(mouseClickPos);
-        RaycastHit hitInfo;
-
-        //check if mouse click pos hits ground
-        if (Physics.Raycast(myRay, out hitInfo, 100, whatIsGround))
-        {
-            //set target pos accessed by seek.cs
-            targetPosition = hitInfo.point;
-            return true;
-        }
-
-        return false;
-    }
-
-    //set player destination
-    public void SetPlayerDestination(Vector3 targetPos)
-    {
-        agent.SetDestination(targetPos);
-        //gameObject.transform.LookAt(agent.steeringTarget);
-    }
-
-    public bool HasPlayerReachedDestination()
-    {
-        if (agent.remainingDistance < 1.0f)
-        {
-            //Debug.Log("agent reached destination");
-            return true;
-        }
-
-        return false;
-    }
-
-    public Vector3 GetAgentPosition()
-    {
-        return agent.transform.position;
-    }
-
-    public void CameraSwitch()
-    {
-        CameraManager.SwitchCamera(); //rework
+        //determine next behaviour
+        if (!hasIntroHappened)              nextBehaviour = IntroductionBehaviour;
+        else if (isPetSleeping)             return;
+        else if (!isReportDelivered)        nextBehaviour = StatusCheckBehaviour;
+        else if(isPetSeeking)               nextBehaviour = SeekBehaviour;
+        else if (isPlayerInKitchen)         nextBehaviour = EatBehaviour;
+        else if (isPlayerInGym)             nextBehaviour = ExerciseBehaviour;
+        else if (isPlayerInBedroom)         nextBehaviour = SleepBehaviour;
+        else if (isPlayerInBathroom)        nextBehaviour = BathroomBehaviour;
+        else if (isPlayerAtTrophyCabinet)   nextBehaviour = ViewTrophyBehaviour;
+        else                                nextBehaviour = WanderBehaviour;
     }
 
     public void SetPlayerPosition(string room = default)
@@ -348,7 +229,6 @@ public class PlayerController : BehaviourStateMachine
                 break;
 
             case "trophycabinet":
-                //Debug.Log("player at trophy cabinet"); //DELETE
                 isPlayerInGym = false;
                 isPlayerInKitchen = false;
                 isPlayerInBedroom = false;
@@ -364,6 +244,78 @@ public class PlayerController : BehaviourStateMachine
                 isPlayerAtTrophyCabinet = false;
                 break;
         }
+    }
+
+    private void PetStartsWalking()
+    {
+        //Debug.Log("idle to walk");
+        if (!AudioManager.AudioManagerInstance.IsAudioPlaying("FootStep"))
+            AudioManager.AudioManagerInstance.PlaySound("FootStep");
+
+        PlayerAnimations.IdleToWalk();
+    }
+
+    private void PetStopsWalking()
+    {
+        PlayerAnimations.WalkToIdle();
+        //bug.Log("Walk to Idle called");
+
+        if (AudioManager.AudioManagerInstance.IsAudioPlaying("FootStep"))
+            AudioManager.AudioManagerInstance.StopSound("FootStep");
+
+        if (isPetSleeping) FaceTarget(bedPos.position);
+        else FaceTarget(Camera.main.transform.position);
+    }
+
+
+    //set player destination
+    public void SetPlayerDestination(Vector3 targetPos)
+    {
+        agent.SetDestination(targetPos);
+    }
+
+    public bool HasPlayerReachedDestination()
+    {
+        if (agent.remainingDistance < 1.0f)
+            return true;
+        return false;
+    }
+
+    public Vector3 GetAgentPosition()
+    {
+        return agent.transform.position;
+    }
+
+    public void CameraSwitch()
+    {
+        CameraManager.SwitchCamera();
+    }
+
+    private void FaceTarget(Vector3 destination)
+    {
+        Vector3 lookPos = destination - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.05f);
+    }
+
+    public void WaitForTimeBeforeSettingDestination(float secs, Vector3 destination = default)
+    {
+        StartCoroutine(WaitBeforeSleepEnds(secs, destination));
+    }
+
+    private IEnumerator WaitBeforeSleepEnds(float secs, Vector3 destination)
+    {
+        yield return new WaitForSeconds(secs);
+        IsPetSleeping(false);
+        SetPlayerDestination(destination);
+    }
+
+    public IEnumerator DelayedCallback(Action<bool> callBack, float secs)
+    {
+        Debug.Log("Delayed Callback successfully called");
+        yield return new WaitForSeconds(secs);
+        callBack(true);
     }
 
     public void IsPetSleeping(bool value)
@@ -391,6 +343,51 @@ public class PlayerController : BehaviourStateMachine
         isPetSeeking = value;
     }
 
+    //===
+    //don't include below in portfolio
+
+    public void ActivateGymGearObjs(bool value)
+    {
+        Sweatband.SetActive(value);
+        RightDumbell.SetActive(value);
+        LeftDumbell.SetActive(value);
+    }
+
+    public void ActivateFoodObj(string foodType, bool value)
+    {
+        if (foodType == "healthy") HealthyFood.SetActive(value);
+        if (foodType == "unhealthy") UnhealthyFood.SetActive(value);
+    }
+
+    public void IncreaseRoomCounter()
+    {
+        roomCounter++;
+        Debug.Log(roomCounter);
+
+        //if (roomCounter > 2 && !UIManager.UIManagerInstance.hasSpiritLevelBeenPressedForFirstTime)
+        //{
+        //    DialogueManager.DialogueManagerInstance.PetConversation("SpiritLevelFirstClick");
+        //    UIManager.UIManagerInstance.SetSpiritLevelFirstClick();
+        //}
+    }
+
+    //check whether input is valid movement target for player
+    private bool isClickPointOnGround(Vector3 mouseClickPos)
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(mouseClickPos);
+        RaycastHit hitInfo;
+
+        //check if mouse click pos hits ground
+        if (Physics.Raycast(myRay, out hitInfo, 100, whatIsGround))
+        {
+            //set target pos accessed by seek.cs
+            targetPosition = hitInfo.point;
+            return true;
+        }
+
+        return false;
+    }
+
     private bool IsPetAbleToWander()
     {
         return false;
@@ -410,57 +407,5 @@ public class PlayerController : BehaviourStateMachine
     private void ResetCountDownTimer()
     {
         countDownTimer = maxCountDown;
-    }
-
-    private void FaceTarget(Vector3 destination)
-    {
-        Vector3 lookPos = destination - transform.position;
-        lookPos.y = 0;
-        Quaternion rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.05f);
-    }
-
-    public void ActivateGymGearObjs(bool value)
-    {
-        Sweatband.SetActive(value);
-        RightDumbell.SetActive(value);
-        LeftDumbell.SetActive(value);
-    }
-
-    public void ActivateFoodObj(string foodType, bool value)
-    {
-        if (foodType == "healthy") HealthyFood.SetActive(value);
-        if (foodType == "unhealthy") UnhealthyFood.SetActive(value);
-    }
-
-    public void WaitForTimeBeforeSettingDestination(float secs, Vector3 destination = default)
-    {
-        StartCoroutine(WaitBeforeSleepEnds(secs, destination));
-    }
-
-    private IEnumerator WaitBeforeSleepEnds(float secs, Vector3 destination)
-    {
-        yield return new WaitForSeconds(secs);
-        IsPetSleeping(false);
-        SetPlayerDestination(destination);
-    }
-
-    public IEnumerator DelayedCallback(Action<bool> callBack, float secs)
-    {
-        Debug.Log("Delayed Callback successfully called");
-        yield return new WaitForSeconds(secs);
-        callBack(true);
-    }
-
-    public void IncreaseRoomCounter()
-    {
-        roomCounter++;
-        Debug.Log(roomCounter);
-
-        //if (roomCounter > 2 && !UIManager.UIManagerInstance.hasSpiritLevelBeenPressedForFirstTime)
-        //{
-        //    DialogueManager.DialogueManagerInstance.PetConversation("SpiritLevelFirstClick");
-        //    UIManager.UIManagerInstance.SetSpiritLevelFirstClick();
-        //}
     }
 }
